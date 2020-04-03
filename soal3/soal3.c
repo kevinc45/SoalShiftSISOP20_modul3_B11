b7fd59c1d097a73fb56465cd5e5495d3361c5f70
Kevin Christian Hadinata <38512540+kevinc45@users.noreply.github.com>
2020-04-03 12:49:42 +0700

    Update soal3.c
    Belum menyerah meskipun tetep nggak selesai

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -3,15 +3,18 @@
 #include <unistd.h> 
 #include <stdlib.h>
 #include <dirent.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <pthread.h> 
-#include <fcntl.h>
 #include <errno.h>
-#include <syslog.h>
-#include <ctype.h>
+#include <sys/stat.h>
 
+char directorymenu[100];
+char directorylama[100];
+char filebaru[100];
+
+const char *get_filename_ext(const char *filename) {
+    const char *dot = strrchr(filename, '.');
+    if(!dot || dot == filename) return "";
+    return dot + 1;
+}
 
 int main(int argc, char **argv){
 
@@ -23,7 +26,7 @@ if (strcmp(argv[1],"-f")==0){
 else if (strcmp(argv[1],"*")==0){
 	char direc[100];	
 	struct dirent *de;
-	DIR *dir = opendir("/home/kevinc45/koleksi");
+	DIR *dir = opendir("/home/kevinc45/koleksi/");
 	
 	if(!dir){
         printf("opendir() failed! Does it exist?\n");
@@ -33,19 +36,27 @@ else if (strcmp(argv[1],"*")==0){
 	unsigned long count=0;
         
 	while(de = readdir(dir)){
-	if (strcmp(de->d_name,".") && strcmp(de->d_name,"..")){
-	strcpy(direc,"/home/kevinc45/");
-	strcat(direc,de->d_name);
-	if(de->d_type == 8){
+	if (de->d_type==4) continue;
 	
-	}
-	++count;
-}
-    	closedir(dir);
-    	
-            }
-    return 0;
+	strcpy(directorylama,"/home/kevinc45/koleksi/");
+	strcat(directorylama,de->d_name);
 
+	strcpy(directorymenu,"/home/kevinc45/");
+	strcat(directorymenu,get_filename_ext(de->d_name));
+	
+	strcpy(filebaru,directorymenu);
+	strcat(filebaru,de->d_name);
+	
+
+	DIR *dir2 = opendir(directorymenu);
+	mkdir(get_filename_ext(de->d_name),0755);		
+	rename(directorylama,filebaru);
+   	closedir(dir2);	
+	count++;
+	}
+
+    	closedir(dir);
+    return 0;
 }
 else if (strcmp(argv[1], "-d") == 0){}
 else{printf("Inputan salah!\n");}
